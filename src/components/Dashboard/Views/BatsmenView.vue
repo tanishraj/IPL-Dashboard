e73326dbbd5c360dae2fcb383cdc114fabf4fdcb
tanishraj <tanishraj.quitegrowth@gmail.com>
2020-07-20 00:57:25 +0530

    Final Commit

--- src/components/Dashboard/Views/BatsmenView.vue
+++ src/components/Dashboard/Views/BatsmenView.vue
@@ -2,16 +2,79 @@
   <div>
     <div class="batsmen">
       <h1 class="title">Tops 10 batsmen</h1>
+      <bar-chart
+        :height="295"
+        :chartData="datacollection"
+        :customOption="customOption"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import BarChart from "../../UIComponents/BarChart";
+
 export default {
-  data() {
-    return {};
+  components: {
+    BarChart,
   },
-  beforeMount() {}
+  data() {
+    return {
+      batsmen: this.$store.getters.particularSeasonTopBatsMen("overAll"),
+      datacollection: {},
+      customOption: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontColor: "white",
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  beforeMount() {
+    this.datacollection = {
+      labels: this.batsmen.map((b) => b.batsman),
+      datasets: [
+        {
+          label: "Total runs (2008-2016)",
+          backgroundColor: "#07d89d",
+          data: this.batsmen.map((b) => b.total_runs),
+        },
+        {
+          label: "Total Matches played in season",
+          backgroundColor: "#81ffdb",
+          data: this.batsmen.map((b) => b.extra_runs),
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/components/Dashboard/Views/BowlersView.vue
+++ src/components/Dashboard/Views/BowlersView.vue
@@ -2,15 +2,79 @@
   <div>
     <div class="bowlers">
       <h1 class="title">Tops 10 Number of Over</h1>
+      <bar-chart
+        :height="300"
+        :chartData="datacollection"
+        :customOption="customOption"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import BarChart from "../../UIComponents/BarChart";
+
 export default {
+  components: {
+    BarChart,
+  },
   data() {
-    return {};
-  }
+    return {
+      bowlers: this.$store.getters.particularSeasonTopOverBowled("overAll"),
+      datacollection: {},
+      customOption: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontColor: "white",
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  beforeMount() {
+    this.datacollection = {
+      labels: this.bowlers.map((b) => b.bowler),
+      datasets: [
+        {
+          label: "Total Overs (2008-2016)",
+          backgroundColor: "#e86c9a",
+          data: this.bowlers.map((b) => b.total_overs),
+        },
+        {
+          label: "Economy",
+          backgroundColor: "#ffc9dd",
+          data: this.bowlers.map((b) => b.economy),
+        },
+      ],
+    };
+  },
 };
 </script>
 
