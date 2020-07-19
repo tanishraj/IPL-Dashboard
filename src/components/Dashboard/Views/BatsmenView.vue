36dcf03a58a15050cf053687710a9d9ae661090e
tanishraj <tanishraj.quitegrowth@gmail.com>
2020-07-19 21:59:13 +0530

    IPL dashboard

---
+++ src/components/Dashboard/Views/BatsmenView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="batsmen">
+      <h1 class="title">Tops 10 batsmen</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  beforeMount() {}
+};
+</script>
+
+<style lang="scss" scoped>
+%text {
+  font-family: "IBM Plex Sans", sans-serif;
+  text-transform: uppercase;
+  color: white;
+}
+.title {
+  @extend %text;
+  padding: 0 9px;
+  font-size: 43px;
+  margin: 0;
+  font-weight: 700;
+  background: linear-gradient(135deg, #fff 10%, #07d89d 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  @media screen and (max-width: 768px) {
+    font-size: 27px;
+  }
+}
+.batsmen {
+  display: block;
+  width: 100%;
+  height: 381px;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  margin-bottom: 21px;
+  border-radius: 5px;
+  position: relative;
+  z-index: 2;
+}
+</style>
---
+++ src/components/Dashboard/Views/BowlersView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="bowlers">
+      <h1 class="title">Tops 10 Number of Over</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+%text {
+  font-family: "IBM Plex Sans", sans-serif;
+  text-transform: uppercase;
+  color: white;
+}
+.title {
+  @extend %text;
+  padding: 0 9px;
+  font-size: 43px;
+  margin: 0;
+  font-weight: 700;
+  background: linear-gradient(135deg, #fff 10%, #e86c9a 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  @media screen and (max-width: 768px) {
+    font-size: 27px;
+  }
+}
+.bowlers {
+  display: block;
+  width: 100%;
+  height: 381px;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  margin-bottom: 21px;
+  border-radius: 5px;
+  position: relative;
+  z-index: 2;
+}
+</style>
